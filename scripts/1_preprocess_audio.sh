44ca0ecc3dc4f2cc230f756b7aac067c8eb8b041
chan <chan.lee.21@ucl.ac.uk>
2025-09-04 21:17:20 +0900

    remove previous comment

--- scripts/1_preprocess_audio.sh
+++ scripts/1_preprocess_audio.sh
@@ -2,7 +2,7 @@
 set -e
 # Read transcript ( find Hi i'm ellie) and find t0
 # save t0 in json file
-# cut timeline after t0 -> adapt in feature files ( DAIC - clnf, covarep   EDAIC - densenet201, mfcc, openface_pose_gaze_au, vgg16)
+
 
 python -m scripts.feature_extract.daicwoz.daic_audio_trim
 
