f90ce8ccf65124bf0eba2ee8692eb4e4cb2a61dc
chan <chan.lee.21@ucl.ac.uk>
2025-09-04 21:15:37 +0900

    add comment

--- scripts/2_extract_features.sh
+++ scripts/2_extract_features.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-# extract egemaps and save in processed file
-# DAIC-WOZ egemaps in processed DAIC-WOZ/ReEgemaps25LLD (300-492 audio 189 participants)
-# E-DAIC egemaps in processed E-DAIC/ReEgemaps25LLD (600-716 audio )
+# cut timeline after t0 -> adapt in feature files ( DAIC - clnf, covarep   EDAIC - densenet201, mfcc, openface_pose_gaze_au, vgg16)
 
-python -m scripts.feature_extract.daicwoz.daic_egemaps_extract 
+python -m scripts.feature_extract.daicwoz.daic_feature_extract
 
-python -m scripts.feature_extract.edaic.edaic_egemaps_extract
+
+python -m scripts.feature_extract.edaic.edaic_feature_extract
