19487e86bb2e6cb784d0051de135af40dd1d80c7
chan <chan.lee.21@ucl.ac.uk>
2025-09-04 21:09:03 +0900

    change the method calling python file

--- scripts/1_preprocess_audio.sh
+++ scripts/1_preprocess_audio.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -e
+# Read transcript ( find Hi i'm ellie) and find t0
+# save t0 in json file
+# cut timeline after t0 -> adapt in feature files ( DAIC - clnf, covarep   EDAIC - densenet201, mfcc, openface_pose_gaze_au, vgg16)
 
+python -m scripts.feature_extract.daicwoz.daic_audio_trim
 
+python -m scripts.feature_extract.daicwoz.daic_feature_extract
 
-
-python ./scripts/preprocess_audio.py b
+python -m scripts.feature_extract.edaic.edaic_feature_extract
