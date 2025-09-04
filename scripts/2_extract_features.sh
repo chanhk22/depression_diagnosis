be728f49c653f12ff7050cc893a6d647acbe6732
chan <chan.lee.21@ucl.ac.uk>
2025-09-04 22:48:24 +0900

    change feature_extract -> feature_mask

--- scripts/2_extract_features.sh
+++ scripts/2_extract_features.sh
@@ -3,10 +3,10 @@ set -e
 
 # cut timeline after t0 -> adapt in feature files ( DAIC - clnf, covarep   EDAIC - densenet201, mfcc, openface_pose_gaze_au, vgg16)
 
-python -m scripts.feature_extract.daicwoz.daic_feature_extract
+python -m scripts.feature_extract.daicwoz.daic_feature_mask
 
 
-python -m scripts.feature_extract.edaic.edaic_feature_extract
+python -m scripts.feature_extract.edaic.edaic_feature_mask
 
 # from preprocessing.clnf_parser, preprocessing.dvlog_visual_parser normalize the data and save in processed/features file
 # CLNF_features.txt -> CLNF_features.npy
